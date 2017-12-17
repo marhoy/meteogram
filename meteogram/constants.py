4b1536be6a63efd143088620e91d6cf57fbf4123
marhoy <martin.hoy@pvv.ntnu.no>
2017-12-17 20:48:42 +0100

    Initial commit

--- meteogram/constants.py
+++ meteogram/constants.py
@@ -4,7 +4,7 @@
 DEFAULT_PLACE = 'Norway/Oslo/Oslo/Godlia'
 DEFAULT_HOURS = 18
 DEFAULT_SYMBOL_INTERVAL = 3
-DEFAULT_LOCALE = 'no_NO.UTF-8'
+DEFAULT_LOCALE = 'nb_NO.UTF-8'
 
 WIND_SYMBOLS_DIR = 'symbols_wind'
 WEATHER_SYMBOLS_DIR = 'symbols_weather'
