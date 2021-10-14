5ce11e8942d0cd3c675ac68111ab2dcf35a524b7
Martin Høy <martin.hoy@dnv.com>
2021-10-14 11:17:56 +0200

    Adapt to new xml format

--- meteogram/get_weather_data.py
+++ meteogram/get_weather_data.py
@@ -9,65 +9,68 @@ from . import constants
 
 
 def _create_url(place=constants.DEFAULT_PLACE):
-    url = 'https://www.yr.no/place/' + place
+    url = "https://www.yr.no/place/" + place
     return url
 
 
 def get_hourly_forecast(place=constants.DEFAULT_PLACE):
-    url = _create_url(place) + '/varsel_time_for_time.xml'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    # url = _create_url(place) + '/varsel_time_for_time.xml'
+    # response = requests.get(url)
+    url = "https://api.met.no/weatherapi/locationforecast/2.0/classic.xml?altitude=132&lat=59.909068&lon=10.8392416"
+    headers = {
+        "Accept": "application/xml",
+        "User-Agent": "https://github.com/marhoy/meteogram",
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "lxml")
 
-    column_names = ['from', 'to', 'symbol', 'temp', 'precip', 'precip_min', 'precip_max',
-                    'wind_dir', 'wind_speed', 'pressure']
-    df = pd.DataFrame(columns=column_names)
+    rows = []
+    for time in soup.find_all("time"):
+        time_from = pd.to_datetime(time["from"])
+        time_to = pd.to_datetime(time["to"])
 
-    for time in soup.tabular.find_all("time"):
-        row = pd.DataFrame([[
-            time['from'],
-            time['to'],
-            time.symbol['var'],
-            time.temperature['value'],
-            time.precipitation['value'],
-            time.precipitation.get('minvalue', 0),
-            time.precipitation.get('maxvalue', 0),
-            time.winddirection['deg'],
-            time.windspeed['mps'],
-            time.pressure['value']
-        ]], columns=column_names)
-        df = df.append(row)
-    df = df.reset_index(drop=True)
+        if time_from == time_to:
+            row = dict()
+            row["temp"] = float(time.temperature["value"])
+            row["wind_dir"] = float(time.winddirection["deg"])
+            row["wind_speed"] = float(time.windspeed["mps"])
+            row["pressure"] = float(time.pressure["value"])
+        elif time_to - time_from == pd.Timedelta("1 hour"):
+            row["from"] = time_from
+            row["to"] = time_to
+            row["symbol"] = time.symbol["code"]
+            row["precip"] = float(time.precipitation["value"])
+            row["precip_min"] = float(time.precipitation["minvalue"])
+            row["precip_max"] = float(time.precipitation["maxvalue"])
+
+            rows.append(row)
+
+    df = pd.DataFrame(rows)
 
     # Change the data type of the different columns
-    df['from'] = pd.to_datetime(df['from'])
-    df['to'] = pd.to_datetime(df['to'])
-    df['temp'] = df['temp'].astype(int)
-    df['precip'] = df['precip'].astype(float)
-    df['precip_min'] = df['precip_min'].astype(float)
-    df['precip_max'] = df['precip_max'].astype(float)
-    df['wind_dir'] = df['wind_dir'].astype(float)
-    df['wind_speed'] = df['wind_speed'].astype(float)
-    df['pressure'] = df['pressure'].astype(float)
+    # df["from"] = pd.to_datetime(df["from"])
+    # df["to"] = pd.to_datetime(df["to"])
 
     # Create new columns with dates that are Matplotlib-friendly
-    df['from_mpl'] = matplotlib.dates.date2num(df['from'].astype(datetime.datetime))
-    df['to_mpl'] = matplotlib.dates.date2num(df['to'].astype(datetime.datetime))
+    df["from_mpl"] = matplotlib.dates.date2num(
+        df["from"]
+    )  # .astype(datetime.datetime))
+    df["to_mpl"] = matplotlib.dates.date2num(df["to"])  # .astype(datetime.datetime))
     return df
 
 
 def get_precip_now(place=constants.DEFAULT_PLACE):
-    url = _create_url(place) + '/varsel_nu.xml'
+    url = _create_url(place) + "/varsel_nu.xml"
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, "lxml")
 
-    column_names = ['time', 'precip']
+    column_names = ["time", "precip"]
     df = pd.DataFrame(columns=column_names)
 
     for time in soup.forecast.find_all("time"):
-        row = pd.DataFrame([[
-            time['from'],
-            time.precipitation['value']
-        ]], columns=column_names)
+        row = pd.DataFrame(
+            [[time["from"], time.precipitation["value"]]], columns=column_names
+        )
         df = df.append(row)
     df = df.reset_index(drop=True)
 
